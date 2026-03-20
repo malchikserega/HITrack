63b13d01ce1f710378c6580e07208d71d148b0c9
Sergei Ovchinnikov <sergey.o.a@gmail.com>
2026-03-20 12:12:54 -0400

    fix doble logging

--- HITrack/core/utils/artifactory.py
+++ HITrack/core/utils/artifactory.py
@@ -19,11 +19,6 @@ from typing import Any, Dict, Generator, List, Optional, Tuple
 import requests
 import yaml
 
-# Configure logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
 logger = logging.getLogger(__name__)
 
 # Configuration
